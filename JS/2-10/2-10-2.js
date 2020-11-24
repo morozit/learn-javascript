f1451d48f413840f1abb9bab3552a3628e9f2e37
Roman <65686226+morozit@users.noreply.github.com>
2020-11-25 00:00:29 +0200

    add

---
+++ JS/2-10/2-10-2.js
@@ -0,0 +1,14 @@
+// Используя конструкцию if..else, напишите код, который будет спрашивать: „Какое «официальное» название JavaScript?“
+
+// Если пользователь вводит «ECMAScript», то показать: «Верно!», в противном случае – отобразить: «Не знаете? ECMAScript!»
+
+
+let name = prompt("Какое «официальное» название JavaScript?", "");
+
+let ECMAScript = "ECMAScript";
+if (name === ECMAScript) {
+  alert("Верно")
+} else {
+  alert("«Не знаете? ECMAScript!»")
+}
+
