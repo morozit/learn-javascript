f49630e8133be42766749fb1bfe7d4c2f9ab6139
Roman <65686226+morozit@users.noreply.github.com>
2020-12-10 14:14:45 +0200

    add

---
+++ JS/zamukania/6-3-7.js
@@ -0,0 +1,16 @@
+// Посмотрите на код. Какой будет результат у вызова на последней строке?
+
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+
+sayHi();
+
+// is not defined
+// Змінна не оголошенна в доступній області видимості
