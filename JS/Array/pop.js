b52dff8c8696698bcddf30bc50064ac2f955090f
Roman <65686226+morozit@users.noreply.github.com>
2021-02-01 16:45:59 +0200

    add

---
+++ JS/Array/pop.js
@@ -0,0 +1,19 @@
+// ! видаляє елементи з кінця масива
+// -----------------------------------------------------------
+let complexArray = [
+  {'name': 'Ivan ', 'order': 12, 'sum': '123.5'},
+  {'name': '    Roman ', 'order': 12, 'sum': '123,5'},
+  {'name': 'Stepan  ', 'order': 12, 'sum': '123,,5'},
+  {'name': '   Petro ', 'order': 12, 'sum': '123. 5'},
+];
+
+let simpleArray = [4, 2, 5, 12, 10, 42, 7, 0, 52, 32, -42];
+// ------------------------------------------------------------
+
+
+complexArray.pop();
+console.log(complexArray);
+
+
+simpleArray.pop();
+console.log(simpleArray);
---
+++ JS/Array/push.js
@@ -0,0 +1,19 @@
+// ! добавляє елементи в кінець масиву
+// -----------------------------------------------------------
+let complexArray = [
+  {'name': 'Ivan ', 'order': 12, 'sum': '123.5'},
+  {'name': '    Roman ', 'order': 12, 'sum': '123,5'},
+  {'name': 'Stepan  ', 'order': 12, 'sum': '123,,5'},
+  {'name': '   Petro ', 'order': 12, 'sum': '123. 5'},
+];
+
+let simpleArray = [4, 2, 5, 12, 10, 42, 7, 0, 52, 32, -42];
+// ------------------------------------------------------------
+
+
+complexArray.push({'name': 'testPush', 'order': 22, 'sum': '999.99'});
+console.log(complexArray);
+
+
+simpleArray.push('testPush');
+console.log(simpleArray);
